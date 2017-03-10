dc78412b7285ad9a323ae80acc2d48ea38cf0c3b
tyler wanek <tylerw@axosoft.com>
2017-03-10 10:53:24 -0700

    fix whitespace in immutability.js

--- src/js-concepts/immutability.js
+++ src/js-concepts/immutability.js
@@ -24,17 +24,21 @@ console.log();
 
 console.log(' -- Mutating an object -- ');
 console.log('Before updating key:', obj1);
+
 obj1.key = 'new value';
+
 console.log('After updating key:', obj1);
 console.log('obj1 === obj1:', obj1 === obj1);
 console.log();
 
 
 console.log(' -- Updating without mutating -- ');
+
 const obj3 = {
   ...obj2,
   a: 3
 };
+
 console.log('obj2:', obj2);
 console.log('obj3:', obj3);
 console.log('obj2 === obj2:', obj2 === obj2);
