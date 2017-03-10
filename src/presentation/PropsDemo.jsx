583833a24d6120dde7c336fa4f0441f0b402ba69
Tyler Wanek <tylerw@axosoft.com>
2017-03-09 22:46:09 -0700

    Build props demo

--- src/presentation/PropsDemo.jsx
+++ src/presentation/PropsDemo.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React from 'React';
 
-const stub = () => <div />;
+const Header = ({ children }) => <h1>{children}</h1>;
 
-export default stub;
+const Content = ({ text }) => <p>{text}</p>;
+
+const PropsDemo = () => {
+  const text = 'Components can be called with custom properties';
+
+  return (
+    <div className='props-demo'>
+      <Header>This is the children property</Header>
+      <Content text={text} />
+    </div>
+  );
+};
+
+export default PropsDemo;
