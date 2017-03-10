f6f3de203eeb6db968be972ae287b3f7960e944b
tyler wanek <tylerw@axosoft.com>
2017-03-10 11:11:10 -0700

    Update vdom example with some pretend output

--- examples/vdom.js
+++ examples/vdom.js
@@ -41,3 +41,75 @@ var renderVDOM = function renderVDOM() {
     )
   );
 };
+
+
+// The output of renderVDOM might look something like this
+var potentialOutput = {
+  elementType: 'html',
+  props: null,
+  children: [
+    {
+      elementType: 'head',
+      props: null,
+      children: [
+        {
+          elementType: 'link',
+          props: {
+            href: '../static/style.css',
+            rel: 'stylesheet'
+          }
+        }
+      ]
+    },
+    {
+      elementType: 'body',
+      props: null,
+      children: [
+        {
+          elementType: 'section',
+          props: {
+            id: 'root'
+          },
+          children: [
+            {
+              elementType: 'div',
+              props: {
+                className: 'my-div'
+              },
+              children: [
+                {
+                  elementType: 'span',
+                  props: null,
+                  children: [
+                    'With\xA0'
+                  ]
+                },
+                {
+                  elementType: 'span',
+                  props: null,
+                  children: [
+                    'some\xA0'
+                  ]
+                },
+                {
+                  elementType: 'span',
+                  props: null,
+                  children: [
+                    'spans.'
+                  ]
+                }
+              ]
+            },
+            {
+              elementType: 'div',
+              props: null,
+              children: [
+                'Another div'
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
